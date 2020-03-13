dc30d0ac88bf89d4e3b7f45602fb4af4c3c64d99
madedanilonovais <danilo.novais@madeinweb.com.br>
2020-03-13 20:56:49 -0300

    encoder to json sample

--- WebApi/main.py
+++ WebApi/main.py
@@ -1,8 +1,34 @@
-from datetime import datetime
+from datetime import datetime, date
 from Models.item import Item
 from flask import Flask, jsonify
 from flask_restful import reqparse, abort, Api, Resource
 import json
+
+class DatetimeEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, (date, datetime)):
+            return obj.isoformat()
+
+item = Item()
+item.Id = 123
+item.Description = 'Some Description'
+item.Date = datetime.now()      
+
+#encoder sample: https://docs.python.org/3/library/json.html
+
+properties = [ p for p in dir(item) if not p.startswith('_') ]
+print(properties)
+
+print(type(item))
+
+x={}
+for p in properties:
+    x[p]=getattr(item,p)
+
+print(x)
+
+x=DatetimeEncoder().encode(item.__dict__)
+print(f'x={x}')
 
 class Notes(Resource):
     __items = []
