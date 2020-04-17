ecf560ba47ab0b5217a9a1a8d3e104528025c7ca
madedanilonovais <danilo.novais@madeinweb.com.br>
2020-04-17 19:44:40 -0300

    code segregation samples implemented

--- WebApi/main.py
+++ WebApi/main.py
@@ -20,47 +20,8 @@ migrate = Migrate(app, db)
 manager = Manager(app)
 
 manager.add_command('db', MigrateCommand)
-
-subs = db.Table('Subs',
-    db.Column('User_Id', db.BigInteger(), db.ForeignKey('Players.Id')),
-    db.Column('Channel_Id', db.BigInteger(), db.ForeignKey('Channels.Id'))
-)
-
-class Wallet(db.Model):
-    __tablename__ = 'Wallets'
-    id = db.Column('Id', db.BigInteger(), primary_key=True)
-    name = db.Column('Name',db.String(50))
-    player_id = db.Column('Player_Id', db.BigInteger(), db.ForeignKey('Players.Id'), unique=True)
-    player = db.relationship('Player', uselist=False)
-
-class Player(db.Model):
-    __tablename__='Players'
-    id = db.Column('Id', db.BigInteger, primary_key=True)
-    name = db.Column('Name', db.String(50))
-    clubs = db.relationship('Club', backref='player')
-    subscriptions = db.relationship('Channel', secondary=subs,backref=db.backref('subscribers'))
-    wallet = db.relationship('Wallet', uselist=False)
-
-    def __init__(self, name):
-        self.name = name
-
-class Club(db.Model):
-    __tablename__ = 'Clubs'
-    id = db.Column('Id', db.BigInteger, primary_key=True)
-    name = db.Column('Name', db.String(50), unique=True)
-    player_id = db.Column('Player_Id', db.BigInteger, db.ForeignKey('Players.Id'))
-
-    def __init__(self, name, player):
-        self.name = name
-        self.player = player
-
-class Channel(db.Model):
-    __tablename__ = 'Channels'
-    id = db.Column('Id', db.BigInteger(), primary_key=True)
-    name = db.Column('Name', db.String(50))
-    
-    def __init__(self, name):
-        self.name = name
+import tables
+definitions = tables.defineDb(db)
 
 '''
 api.add_resource(NotesList, '/notes')
